c16dde3f4338c66d5d358349cce3b7b1d644f66f
Parthib Chowdhury <parthibchowdhury31@gmail.com>
2023-06-27 23:13:45 +0530

    Day24

---
+++ Day24/Pascal_Triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> v1;
+
+        for(int i=0;i<numRows;i++)
+        {
+            vector<int> v2(i+1,1);
+            for(int j=1;j<i;j++)
+            {
+                int e=v1[i-1][j-1]+v1[i-1][j];
+                v2[j]=e;
+            }
+            v1.push_back(v2);
+        }
+        return v1;
+    }
+};
